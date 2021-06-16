58bbd5de84f45e102dbc892b6b33fd815fb4da41
pondkarun <pondkarun@gmail.com>
2021-06-17 01:27:09 +0700

    sequelize

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -1,68 +1,10 @@
 import { Request, Response, NextFunction } from "express";
 import config from '../config'
 import { sequelize } from '../models'
-import { Model, DataTypes } from 'sequelize';
+import { initModels, jobs } from "../models/init-models";
 
-interface UserInstance extends Model {
-    id: string;
-    code: string;
-    job_name?: string;
-    is_use?: boolean;
-    created_date: Date;
-    updated_date?: Date;
-    password?: string;
-}
-
-const jobs = sequelize.define<UserInstance>("job", {
-    id: {
-        type: DataTypes.UUID,
-        allowNull: false,
-        primaryKey: true
-    },
-    code: {
-        type: DataTypes.STRING(10),
-        allowNull: false,
-        comment: "รหัสงาน"
-    },
-    job_name: {
-        type: DataTypes.STRING(150),
-        allowNull: true,
-        comment: "ชื่องาน"
-    },
-    is_use: {
-        type: DataTypes.BOOLEAN,
-        allowNull: true,
-        defaultValue: true
-    },
-    created_date: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        comment: "สร้างข้อมูลเมื่อ"
-    },
-    updated_date: {
-        type: DataTypes.DATE,
-        allowNull: true,
-        comment: "ปรับปรุงข้อมูลเมื่อ"
-    },
-    password: {
-        type: DataTypes.STRING(255),
-        allowNull: true,
-        comment: "รหัสผ่าน"
-    }
-}, {
-    tableName: 'jobs',
-    schema: 'public',
-    timestamps: false,
-    indexes: [
-        {
-            name: "jobs_pkey",
-            unique: true,
-            fields: [
-                { name: "id" },
-            ]
-        },
-    ]
-});
+// import models into sequelize instance
+initModels(sequelize);
 
 export const login = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -71,10 +13,15 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
         // error.statusCode = config.STATUS_CODE_ERROR;
         // throw error;
 
-        const job = await jobs.findAll();
+        /* Query Model */
+        const items = await jobs.findAll()
+
+        /* Query String */
+        const items2 = await sequelize.query("SELECT * FROM jobs")
 
         return res.status(config.STATUS_CODE).json({
-            items: job,
+            items,
+            items2,
             status_code: 200,
         });
     } catch (error) {
