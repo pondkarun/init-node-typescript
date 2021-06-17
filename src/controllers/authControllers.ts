d21e885f91214facc553d8181c2e1911e281d825
pondkarun <pondkarun@gmail.com>
2021-06-17 22:59:50 +0700

    function กลาง

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import config from '../config'
 import { sequelize } from '../models'
 import { initModels, jobs } from "../models/init-models";
+import { sequelizeString, sequelizeStringFindOne } from '../util';
 
 // import models into sequelize instance
 initModels(sequelize);
@@ -17,11 +18,15 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
         const items = await jobs.findAll()
 
         /* Query String */
-        const items2 = await sequelize.query("SELECT * FROM jobs")
+        const items2 = await sequelizeString("SELECT * FROM jobs")
+        const items3 = await sequelizeStringFindOne("SELECT * FROM jobs where id = '00a95e91-7d6e-418e-b619-e1712721a04b' ")
+        const items4 = await sequelizeStringFindOne("SELECT * FROM jobs where id = $1", ["36893837-d8c3-4c3a-a9ae-96d9ce903ecc"])
 
         return res.status(config.STATUS_CODE).json({
             items,
             items2,
+            items3,
+            items4,
             status_code: 200,
         });
     } catch (error) {
